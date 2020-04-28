a35e31ba1f7836cbd5f0671203e637530f4e9d40
KPuech <puech@DESKTOP-L0AMCHA.localdomain>
2020-04-28 19:49:18 +0200

    resolution.jl de jeu1 (fonctionnel pour l'exemple)

--- jeu1/src/resolution.jl
+++ jeu1/src/resolution.jl
@@ -16,18 +16,42 @@ function cplexSolve(t::Array{Int, 2})
     m = Model(with_optimizer(CPLEX.Optimizer))
 
     # x[i, j, k,l] = 1 if cell (i, j) is visible from (k,l)
-    @variable(m, x[1:n, 1:n, 1:4], Bin) 
-        # 1:haut, 2:gauche, 3:bas, 4:droite
+    @variable(m, x[1:n, 1:n, 1:n, 1:n], Bin) 
+        # x_ijkl = 1 si t_ij > t_kl, 0 sinon
 	
 
     # Each clue around the edge gives the number of towers that are visibles when looking into the grid from that direction
-    @constraint(m, [j in 1:n], sum(x[i,j,1] for i in 1:n) == t[1,j+1])
-    @constraint(m, [i in 1:n], sum(x[i,j,2] for j in 1:n) == t[i+1,1])
-    @constraint(m, [j in 1:n], sum(x[i,j,3] for i in 1:n) == t[n+2,j+1])
-    @constraint(m, [i in 1:n], sum(x[i,j,4] for j in 1:n) == t[i+1,n+2])
+    @constraint(m, [j in 1:n], sum((1/(i-1))*sum(x[i,j,k,j] for k in 1:(i-1)) for i in 2:n) + 1 >= t[1,j+1])
+    @constraint(m, [i in 1:n], sum((1/(j-1))*sum(x[i,j,i,k] for k in 1:(j-1)) for j in 2:n) + 1 >= t[i+1,1])
+    @constraint(m, [j in 1:n], sum((1/(n-i))*sum(x[i,j,k,j] for k in (i+1):n) for i in 1:(n-1)) + 1 >= t[n+2,j+1])
+    @constraint(m, [i in 1:n], sum((1/(n-j))*sum(x[i,j,i,k] for k in (j+1):n) for j in 1:(n-1)) + 1 >= t[i+1,n+2])
+
+    # sum(x[i,j,i,k] for k in 1:n) + 1 = t_ij
+    @constraint(m, [i in 1:n, j in 1:n], sum(x[i,j,i,k] for k in 1:n) - sum(x[i,j,k,j] for k in 1:n) == 0)
+    @constraint(m, [i in 1:n, j in 1:n], sum(x[i,j,i,k] for k in 1:n) <= n - 1)
+    @constraint(m, [i in 1:n, j in 1:n], sum(x[i,j,i,k] for k in 1:n) >= 0)
+
+    @constraint(m, [i in 1:n, j in 1:n], x[i,j,i,j] == 0)
+    @constraint(m, [i in 1:n, j in 2:n, l in 1:(j-1)], x[i,l,i,j] + x[i,j,i,l] == 1)
+    @constraint(m, [i in 1:n, j in 1:(n-1), l in (j+1):n], x[i,l,i,j] + x[i,j,i,l] == 1)
+    @constraint(m, [i in 1:n, j in 2:n, k in 1:(i-1)], x[k,j,i,j] + x[i,j,k,j] == 1)
+    @constraint(m, [i in 1:n, j in 1:(n-1), k in (i+1):n], x[k,j,i,j] + x[i,j,k,j] == 1)
+
+    # somme t_ij sur ligne/col doit etre egale a sum(k)
+        # somme des coef sur ligne i
+    @constraint(m, [i in 1:n], sum(sum(x[i,j,i,k] for k in 1:n) + 1 for j in 1:n) == sum(k for k in 1:n))
+        # somme des coef sur col j
+    @constraint(m, [j in 1:n], sum(sum(x[i,j,i,k] for k in 1:n) + 1 for i in 1:n) == sum(k for k in 1:n))
+
+    @constraint(m, [i in 1:n, j in 1:n, k in 1:n, l in 1:n; k!=l && j!=l && k!=j], x[i,j,i,k] + x[i,k,i,l] - 1 <= x[i,j,i,l])
+    @constraint(m, [i in 1:n, j in 1:n, k in 1:n, l in 1:n; k!=l && i!=k && l!=i], x[i,j,k,j] + x[k,j,l,j] - 1 <= x[i,j,l,j])
+
 
     # Maximize the top-left cell
-    @objective(m, Max, sum(x[1, 1, k] for k in 1:4))
+    @objective(m, Min, sum(sum((1/(i-1))*sum(x[i,j,k,j] for k in 1:(i-1)) for i in 2:n) for j in 1:n) 
+                        + sum(sum((1/(j-1))*sum(x[i,j,i,k] for k in 1:(j-1)) for j in 2:n) for i in 1:n) 
+                        + sum(sum((1/(n-i))*sum(x[i,j,k,j] for k in (i+1):n) for i in 1:(n-1)) for j in 1:n) 
+                        + sum(sum((1/(n-j))*sum(x[i,j,i,k] for k in (j+1):n) for j in 1:(n-1)) for i in 1:n))
 
     # Start a chronometer
     start = time()
@@ -35,60 +59,10 @@ function cplexSolve(t::Array{Int, 2})
     # Solve the model
     optimize!(m)
 
-    #Test passage de x a t avec une deuxieme application de optimize
-    m2 = Model(with_optimizer(CPLEX.Optimizer))
-    @variable(m2, 1 <= s[1:n, 1:n] <= n, Int) 
-
-    for i in 1:n
-        for j in 1:n
-            if x[i,j,1] == 1
-                if i > 1
-                    for k in 1:(i-1)
-                        @constraint(m2, s[k,j] - s[i,j] <= -1)
-                    end
-                end
-            end
-
-            if x[i,j,2] == 1
-                if j > 1
-                    for k in 1:(j-1)
-                        @constraint(m2, s[i,k] - s[i,j] <= -1)
-                    end
-                end
-            end
-
-            if x[i,j,3] == 1
-                if i < n
-                    for k in (i+1):n
-                        @constraint(m2, s[k,j] - s[i,j] <= -1)
-                    end
-                end
-            end
-
-            if x[i,j,4] == 1
-                if j < n
-                    for k in (j+1):n
-                        @constraint(m2, s[i,k] - s[i,j] <= -1)
-                    end
-                end
-            end
-
-            if (x[i,j,1] == 1) && (x[i,j,2] == 1) && (x[i,j,3] == 1) && (x[i,j,4] == 1)
-                @constraint(m2, s[i,j] == n)       
-            end
-        end
-    end
-
-    # Maximize the top-left cell
-    @objective(m2, Max, s[1, 1])
-
-    # Solve the model
-    optimize!(m2)
-
     # Return:
     # 1 - true if an optimum is found
     # 2 - the resolution time
-    return JuMP.primal_status(m) == JuMP.MathOptInterface.FEASIBLE_POINT, x, s, time() - start
+    return JuMP.primal_status(m) == JuMP.MathOptInterface.FEASIBLE_POINT, x, time() - start
     
 end
 
@@ -133,10 +107,13 @@ function solveDataSet()
 
     # For each instance
     # (for each file in folder dataFolder which ends by ".txt")
-    for file in filter(x->occursin(".txt", x), readdir(dataFolder))
+    for file in filter(x->occursin(".txt", x), readdir(dataFolder))  
         
         println("-- Resolution of ", file)
-       t= readInputFile(dataFolder * file)
+        readInputFile(dataFolder * file)
+
+        # TODO
+        println("In file resolution.jl, in method solveDataSet(), TODO: read value returned by readInputFile()")
         
         # For each resolution method
         for methodId in 1:size(resolutionMethod, 1)
@@ -146,7 +123,7 @@ function solveDataSet()
             # If the instance has not already been solved by this method
             if !isfile(outputFile)
                 
-                fout = open(outputFile, "w")
+                fout = open(outputFile, "w")  
 
                 resolutionTime = -1
                 isOptimal = false
@@ -154,15 +131,16 @@ function solveDataSet()
                 # If the method is cplex
                 if resolutionMethod[methodId] == "cplex"
                     
+                    # TODO 
+                    println("In file resolution.jl, in method solveDataSet(), TODO: fix cplexSolve() arguments and returned values")
+                    
                     # Solve it and get the results
-                    isOptimal=cplexSolve(t)[1]
-                    x=cplexSolve(t)[2]
-                    resolutionTime = cplexSolve(t)[3]
+                    isOptimal, resolutionTime = cplexSolve()
                     
                     # If a solution is found, write it
                     if isOptimal
                         # TODO
-                       writeSolution(fout,x)
+                        println("In file resolution.jl, in method solveDataSet(), TODO: write cplex solution in fout") 
                     end
 
                 # If the method is one of the heuristics
@@ -170,13 +148,13 @@ function solveDataSet()
                     
                     isSolved = false
 
-                    # Start a chronometer
+                    # Start a chronometer 
                     startingTime = time()
                     
                     # While the grid is not solved and less than 100 seconds are elapsed
                     while !isOptimal && resolutionTime < 100
                         
-                        # TODO
+                        # TODO 
                         println("In file resolution.jl, in method solveDataSet(), TODO: fix heuristicSolve() arguments and returned values")
                         
                         # Solve it and get the results
@@ -193,14 +171,14 @@ function solveDataSet()
                         # TODO
                         println("In file resolution.jl, in method solveDataSet(), TODO: write the heuristic solution in fout")
                         
-                    end
+                    end 
                 end
 
-                println(fout, "solveTime = ", resolutionTime)
+                println(fout, "solveTime = ", resolutionTime) 
                 println(fout, "isOptimal = ", isOptimal)
                 
                 # TODO
-                println("In file resolution.jl, in method solveDataSet(), TODO: write the solution in fout")
+                println("In file resolution.jl, in method solveDataSet(), TODO: write the solution in fout") 
                 close(fout)
             end
 
@@ -209,6 +187,6 @@ function solveDataSet()
             include(outputFile)
             println(resolutionMethod[methodId], " optimal: ", isOptimal)
             println(resolutionMethod[methodId], " time: " * string(round(solveTime, sigdigits=2)) * "s\n")
-        end
-    end
+        end         
+    end 
 end
